96a3e5633fba0607c3f398f5ed8cef0e668edd35
Harrisonkamau <kamauharrison87@gmail.com>
2016-07-21 08:36:09 +0300

    create a function to validate phone

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
 function validate(){
-  if(form_email.email.value==""){
+  if(form_validate.email.value==""){
     alert("Please enter an email!");
-    form_email.email.focus();
+    form_validate.email.focus();
     return false;
   }
   var regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-  if(regex.test(form_email.email.value)==false){
+  if(regex.test(form_validate.email.value)==false){
     alert('Invalid email address');
-    form_email.email.focus();
+    form_validate.email.focus();
     return false;
   }
 return true;
+}
+
+function validatePhone() {
+  var phoneNumber = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
+  if(form_validate.contact.value.match(phoneNumber)) {
+    return true;
+  }
+  else {
+    alert("Phone number is required!");
+    return false;
+  }
 }
